3a914711845404e420cb0b988381fb2df6d219c6
Nitish Raj Moganti (nmoganti) <nmoganti@cisco.com>
2023-11-03 10:22:24 +0530

    Removed all lines causing error

--- run_app.bat
+++ run_app.bat
@@ -1,7 +1,7 @@
 @echo off
-@REM docker run crashserver
-timeout /t 2 >nul
-for /f %%i in ('docker ps -l -q') do set container_id=%%i
-echo server container id: %container_id%
-echo to stop the server run the following command: docker kill %container_id%
+docker run crashserver
+@REM timeout /t 2 >nul
+@REM for /f %%i in ('docker ps -l -q') do set container_id=%%i
+@REM echo server container id: %container_id%
+@REM echo to stop the server run the following command: docker kill %container_id%
 
