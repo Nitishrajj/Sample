3c8e5ab89b8d08f5541195c2357ae8cb88b28cf2
Nitish Raj Moganti (nmoganti) <nmoganti@cisco.com>
2023-11-03 10:17:11 +0530

    Edited run_app.bat file

--- run_app.bat
+++ run_app.bat
@@ -1,6 +1,7 @@
 @echo off
 docker run crashserver
 timeout /t 2 >nul
-for /f "tokens=*" %%i in ('docker ps -l -q') do set container_id=%%i
+for /f %%i in ('docker ps -l -q') do set container_id=%%i
 echo server container id: %container_id%
-echo to stop server run the following command: docker kill %container_id%
+echo to stop the server run the following command: docker kill %container_id%
+
